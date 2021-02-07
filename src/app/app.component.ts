6485cfa4d65f1ebd6fc157e96814d1309ee332b0
Yevgeny Levin <devlev1980@gmail.com>
2021-02-07 12:33:35 +0200

    add card component add accordion component add progress-bar component add star-ratings component add top-of-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IAccordionModel} from './components/accordion/accordion.model';
 
 @Component({
   selector: 'yl-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-challanges';
+  progressValue: number = 0;
+  accordionItems: IAccordionModel[] = [
+    {
+      id: 0,
+      title: 'Example 1',
+      content: 'Example Content 1',
+      isExpanded: false
+    },
+    {
+      id: 2,
+      title: 'Example 2',
+      content: 'Example Content 2',
+      isExpanded: false
+    }
+  ];
 }
---
+++ src/app/components/accordion/accordion.model.ts
@@ -0,0 +1,6 @@
+export interface IAccordionModel{
+  id: number;
+  title: string;
+  content: string;
+  isExpanded: boolean;
+}
