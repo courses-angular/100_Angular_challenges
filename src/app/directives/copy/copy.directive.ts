083826beb2d75ad3ed9503d8dd751f301761d52b
Yevgeny Levin <devlev1980@gmail.com>
2021-02-14 13:45:01 +0200

    add copy directive

--- src/app/directives/copy/copy.directive.ts
+++ src/app/directives/copy/copy.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {Directive, HostListener, Inject, Input} from '@angular/core';
+import {SnackbarService} from '../../services/snackbar/snackbar.service';
 
 @Directive({
   selector: '[ylCopy]'
 })
 export class CopyDirective {
+  @Input() appCopy: string = '';
 
-  constructor() { }
+  constructor(@Inject('Navigator') private  navigator: Navigator,
+              @Inject('Document') private document: Document,
+              private snackbarService: SnackbarService) {
+  }
+
+  @HostListener('click')
+  async copy(): Promise<void> {
+    try {
+      await this.navigator.clipboard.writeText(this.appCopy);
+      this.document.execCommand('copy');
+      this.snackbarService.callSnackbar('Copied successfully');
+    } catch (e) {
+      this.snackbarService.callSnackbar('Copy failed');
+    }
+  }
 
 }
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,25 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { DirectivesRoutingModule } from './directives-routing.module';
-import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
+import {DirectivesRoutingModule} from './directives-routing.module';
+import {DebounceClickDirective} from './debounce-click/debounce-click.directive';
 import {SharedModule} from '../shared/shared.module';
-import { RippleDirective } from './ripple/ripple.directive';
+import {RippleDirective} from './ripple/ripple.directive';
 import {DirectivesDocumentationComponent} from './directives-documentation/directives-documentation.component';
-import { ScaleDirective } from './scale/scale.directive';
-import { CopyDirective } from './copy/copy.directive';
+import {ScaleDirective} from './scale/scale.directive';
+import {CopyDirective} from './copy/copy.directive';
+import { LazyLoadImageDirective } from './lazy-load-image/lazy-load-image.directive';
 
 
 @NgModule({
-  declarations: [DirectivesDocumentationComponent,DebounceClickDirective, RippleDirective, ScaleDirective, CopyDirective],
-    exports: [
-        DebounceClickDirective,
-        RippleDirective
-    ],
+  declarations: [DirectivesDocumentationComponent, DebounceClickDirective, RippleDirective, ScaleDirective, CopyDirective, LazyLoadImageDirective],
+  exports: [
+    DebounceClickDirective,
+    RippleDirective
+  ],
   imports: [
     CommonModule,
     DirectivesRoutingModule,
     SharedModule
+  ],
+  providers: [
+    {
+      provide: 'Navigator',
+      useValue: navigator
+    },
+    {
+      provide: 'Document',
+      useValue: document
+    }
   ]
 })
-export class DirectivesModule { }
+export class DirectivesModule {
+}
